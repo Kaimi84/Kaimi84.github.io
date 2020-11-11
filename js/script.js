3aa095480139b9173a3a060d39b186d64c20404f
Kaimi84 <you@example.com>
2020-11-11 15:44:28 +0100

    Old files have been modified and new files have been added.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*
+$(document).ready(function() {
+    "use strict";
+  
+    $('ul.navbar-nav > li').click(function(e) {
+      e.preventDefault();
+      $('ul.navbar-nav > li').removeClass('active');
+      $(this).addClass('active');
+    });
+  });
+  */
+
+ /*<script>
+ $(function() {
+    $('#nav li a').click(function() {
+       $('#nav li').removeClass();
+       $($(this).attr('href')).addClass('active');
+    });
+ });
+</script>*/
